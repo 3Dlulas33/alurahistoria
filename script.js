9b1ed8bf6190e71a02919ffb286d2901a1a98290
3Dlulas33 <168127136+3Dlulas33@users.noreply.github.com>
2024-09-25 08:51:19 -0300

    adicionando arquivos

---
+++ script.js
@@ -0,0 +1,11 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+//Luiz Eduardo Almeida Mororo 3D 25/09/2024
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
